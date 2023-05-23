f5f9cb9832663498d323af48a1c1912d3915d62e
“dullashravya” <Nagoor1247@gmail.com>
2023-05-23 16:53:37 +0530

    fixed

--- dotnet/MyOrganization/Organization.cs
+++ dotnet/MyOrganization/Organization.cs
@@ -14,7 +14,13 @@ namespace MyOrganization
         }
 
         protected abstract Position CreateOrganization();
+//         In the Hire method, we first check whether the person or the title is null or empty. If they are, we throw an exception as we cannot hire a person without a name or for a non-existing position.
 
+// Next, we use a helper method, HireHelper, to recursively navigate through the positions. This helper method takes a position, a person and a title. It first checks whether the title of the current position matches the given title and whether the position is already filled. If the title matches and the position is not filled, it creates a new employee and sets this employee for the current position.
+
+// If the title does not match or the position is already filled, the method recursively calls itself for each of the direct reports of the current position.
+
+// If a suitable position is found in one of the direct reports, it is returned. If no suitable position is found in the entire organization, the method returns null.
         public Position? Hire(Name person, string title)
         {
             if (person == null)
